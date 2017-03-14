d7d65f6a4c17df6c61fcb20f3a31d04b60c9aa6a
cprudhom <cprudhom@mines-nantes.fr>
2017-03-14 17:52:31 +0100

    add heat color map

--- src/main/java/org/chocosolver/gui/panels/ColorVariablesPanel.java
+++ src/main/java/org/chocosolver/gui/panels/ColorVariablesPanel.java
@@ -9,11 +9,14 @@
 package org.chocosolver.gui.panels;
 
 import org.chocosolver.gui.GUI;
+import org.chocosolver.solver.search.loop.monitors.IMonitorInitialize;
 import org.chocosolver.solver.search.loop.monitors.IMonitorOpenNode;
+import org.chocosolver.solver.variables.IntVar;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+
+import javax.swing.*;
 
 /**
  * <br/>
@@ -21,7 +24,7 @@ import java.awt.image.BufferedImage;
  * @author Charles Prud'homme
  * @since 05/06/2014
  */
-public class ColorVariablesPanel extends APanel implements IMonitorOpenNode {
+public class ColorVariablesPanel extends APanel implements IMonitorOpenNode, IMonitorInitialize {
 
     int size;
     int psize = 10;
@@ -29,6 +32,8 @@ public class ColorVariablesPanel extends APanel implements IMonitorOpenNode {
     BufferedImage image;
     JLabel theLabel;
     ImageIcon icon;
+    IntVar[] ivars;
+    float[] ids;
 
 
     public ColorVariablesPanel(GUI frame) {
@@ -41,25 +46,46 @@ public class ColorVariablesPanel extends APanel implements IMonitorOpenNode {
         icon = new ImageIcon(image);
         theLabel = new JLabel(new ImageIcon(image));
         add(theLabel);
+        afterInitialize();
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        for (int i = 0; i < model.getNbVars(); i++) {
+        for(int i = 0 ; i < ivars.length; i++){
             int x = i / size;
             int y = i % size;
-            Color color = model.getVar(i).isInstantiated() ? Color.GREEN : Color.BLUE;
+            float[] rgb = rgb(ivars[i].getDomainSize() / ids[i]);
+            Color color = new Color(rgb[0], rgb[1], rgb[2]);
             for (int j = 0; j < psize; j++)
                 for (int k = 0; k < psize; k++)
                     image.setRGB(x * psize + j, y * psize + k, color.getRGB());
         }
     }
 
+    private static float[] rgb(float value){
+        int aR = 0;   int aG = 255; int aB=0;  // RGB for our 1st color (blue in this case).
+        int bR = 0; int bG = 0; int bB=255;    // RGB for our 2nd color (red in this case).
+        float[] rgb = new float[3];
+        rgb[0]  = ((bR - aR) * value + aR) / 255f;      // Evaluated as -255*value + 255.
+        rgb[1] = ((bG - aG) * value + aG) / 255f;      // Evaluates as 0.
+        rgb[2]  =  ((bB - aB) * value + aB) / 255f;       // Evaluates as 255*value + 0.
+        return rgb;
+    }
+
     @Override
     public void plug(JTabbedPane tabbedpanel) {
         super.plug(tabbedpanel);
         tabbedpanel.addTab("Color map", this);
+    }
+
+    @Override
+    public void afterInitialize() {
+        ivars = model.retrieveIntVars(true);
+        ids = new float[ivars.length];
+        for(int i = 0 ; i < ivars.length; i++){
+            ids[i] = 1.f * ivars[i].getDomainSize();
+        }
     }
 
     @Override
